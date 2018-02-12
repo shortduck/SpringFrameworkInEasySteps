d5a05e62c46b9800aadd4c7f18c3f2b7aac70b1a
Bhurat <bhurata@michigan.gov>
2018-02-12 12:01:42 -0500

    .

---
+++ SpringMVC/src/main/java/me/ankit/spring/springmvc/manning/controller/HelloController.java
@@ -0,0 +1,13 @@
+package me.ankit.spring.springmvc.manning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/")
+	public String kuch_bhi() {
+		return "hello";
+	}
+}
